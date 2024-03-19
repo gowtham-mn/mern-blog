640cea6aa03bc5d144685d1cc6724c8a84a0a7a7
gowtham <gowthamsjbit@gmail.com>
2024-03-19 13:20:58 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { signInSuccess, signInFailure, signInStart } from "../redux/user/userSlice";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
 
 export default function SignIp() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const {loading, error: errorMessage} = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -15,11 +19,10 @@ export default function SignIp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields");
+      return dispatch(signInFailure("Please fill out all fields"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -30,20 +33,18 @@ export default function SignIp() {
 
       const data = await res.json();
 
-      setLoading(false);
-
       if (data.success == false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
 
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/');
       }
 
       
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
 
