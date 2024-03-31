4246ccfbf51e7dba3993e2496c0ed83db97946f8
gowtham <gowthamsjbit@gmail.com>
2024-03-31 11:31:03 +0530

    add call to action to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallTooAction from "../components/CallTooAction";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -53,12 +54,24 @@ export default function PostPage() {
           {post && post.category}
         </Button>
       </Link>
-      <img src={post && post.image} alt={post && post.title} className="mt-10 p-3 max-h-[600px] w-full object-cover"/>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+      />
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className="italic">{post && (post.content.length /1000).toFixed(0)} mins read</span>
+        <span className="italic">
+          {post && (post.content.length / 1000).toFixed(0)} mins read
+        </span>
       </div>
-      <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}></div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallTooAction/>
+      </div>
     </main>
   );
 }
